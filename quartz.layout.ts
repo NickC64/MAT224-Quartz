463084dfc7f10620f6da9fd0dfae75157a93e799
Nick Chen <97940747+NickC64@users.noreply.github.com>
2024-07-06 22:33:19 -0400

    Quartz sync: Jul 6, 2024, 10:33 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -26,11 +26,10 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.Explorer(),
   ],
   right: [
     Component.Graph(),
-    Component.Explorer(),
     Component.Backlinks(),
   ],
 }
